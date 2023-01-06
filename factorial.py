cd833d47b1f5f8fb532f534cd1ba79e16b4c171a
Arjun Vijayan <vijayanarjun4@gmail.com>
2023-01-06 16:37:52 +0530

    Initial commit

---
+++ factorial.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'extraLongFactorials' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def extraLongFactorials(n):
+    # Write your code here
+    result = 1
+    while n>0:
+        result = result*n
+        n-=1
+    print(result)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    extraLongFactorials(n)
